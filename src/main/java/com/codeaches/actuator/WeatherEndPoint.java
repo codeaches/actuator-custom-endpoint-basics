c12db9a11cadbe53a594d19aab8fd70593a9dc36
codeaches <info001@codeaches.com>
2019-12-28 21:35:37 -0700

    Spring Boot Actuator Custom Endpoint Example [http://codeaches.com/]

--- src/main/java/com/codeaches/actuator/WeatherEndPoint.java
+++ src/main/java/com/codeaches/actuator/WeatherEndPoint.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
